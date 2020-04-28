b62b6bc04af659db372c5495dac766ff81978076
github username <github email address>
2020-04-27 17:40:50 -0700

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -4,81 +4,80 @@ const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 const main = document.body.querySelector("main")
 
-// const img = document.createElement("img")
-// img.src = "../assets/pokemon_teams.gif"
-// main.appendChild(img)
-
 function loadTrainers() {
     fetch(TRAINERS_URL)
     .then( resp => resp.json() )
-    .then( trainers => addTrainers(trainers) )
+    .then( trainers => trainers.forEach(trainer => addTrainer(trainer)) )
 }
 
-function addTrainers(trainers) {
-    trainers.forEach(trainer => {
-        let trainerDiv = document.createElement("div")
-        trainerDiv.id = trainer.id
-        trainerDiv.className = "card"
+function addTrainer(trainer) {
+    let trainerDiv = document.createElement("div")
+    trainerDiv.className = "card"
+    trainerDiv.setAttribute("data-id", trainer.id)
 
-        let p = document.createElement("p")
-        p.innerText = trainer.name
+    let p = document.createElement("p")
+    p.innerText = trainer.name
 
-        let ul = document.createElement("ul")
+    let ul = document.createElement("ul")
+    trainer.pokemons.forEach(pokemon => loadPokemon(ul, pokemon))
+    
+    let btn = document.createElement("button")
+    btn.innerText = "Add Pokemon"
+    btn.setAttribute("data-trainer-id", trainer.id)
+    
+    btn.onclick = () => handleAdd(trainer, ul)
 
-        trainer.pokemons.forEach(pokemon => {
-            let li = document.createElement("li")
-            li.innerText = `${pokemon.nickname} (${pokemon.species})`
+    trainerDiv.appendChild(p)
+    trainerDiv.appendChild(btn)
+    trainerDiv.appendChild(ul)
+    main.appendChild(trainerDiv)
+}
 
-            let releaseBtn = document.createElement("button")
-            releaseBtn.innerText = "Release Pokemon"
-            releaseBtn.className = "release"
-            releaseBtn["data-pokemon-id"] = "pokemon.id"
+function loadPokemon(ul, pokemon) {
+    let li = document.createElement("li")
+    li.innerText = `${pokemon.nickname} (${pokemon.species})`
 
-            releaseBtn.addEventListener("click", () => {
-                fetch((`${POKEMONS_URL}/${pokemon.id}`), {
-                    method: "DELETE",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept": "application/json"
-                    }
-                })
-                .then(resp => resp.json())
-                .then(console.log)
-                .catch(console.log)
+    let releaseBtn = document.createElement("button")
+    releaseBtn.innerText = "Release"
+    releaseBtn.className = "release"
+    releaseBtn.setAttribute("data-pokemon-id", pokemon.id)
+    releaseBtn.onclick = () => handleRelease(pokemon, li)
+
+    li.appendChild(releaseBtn)
+    ul.appendChild(li)
+}
+
+function handleAdd(trainer, ul) {
+    if (ul.getElementsByTagName("li").length < 6) {
+        return fetch(POKEMONS_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                trainer_id: trainer.id
             })
-
-            li.appendChild(releaseBtn)
-            ul.appendChild(li)
         })
+        .then(resp => resp.json())
+        .then(pokemon => loadPokemon(ul, pokemon))
+        .catch(console.log)
+    } else {
+        alert(`${trainer.name}'s team is already full!`)
+    }
+}
 
-        let addBtn = document.createElement("button")
-        addBtn.innerText = "Add Pokemon"
-        addBtn["data-trainer-id"] = trainer.id
-
-        addBtn.addEventListener("click", () => {
-            if (ul.getElementsByTagName("li").length < 6) {
-                fetch(POKEMONS_URL, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept": "application/json"
-                    },
-                    body: JSON.stringify({
-                        trainer_id: trainer.id
-                    })
-                })
-                .then(resp => resp.json())
-                .then(console.log)
-            } else {
-                console.log("You already have six pokemon")
-            }
-        })
-
-        trainerDiv.appendChild(p)
-        trainerDiv.appendChild(addBtn)
-        trainerDiv.appendChild(ul)
-        main.appendChild(trainerDiv)
-    });
+function handleRelease(pokemon, li) {
+    fetch((`${POKEMONS_URL}/${pokemon.id}`), {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    })
+    .then(resp => resp.json())
+    .then(li.remove())
+    .catch(console.log)
 }
 
 loadTrainers()
